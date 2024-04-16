ee25be9ea0cc04edeb7d76e69f2babf0b8a2d495
AbdoulayeXYZ <niasseabdoulaye64@gmail.com>
2024-04-16 23:16:41 +0000

    routes for about and contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './core/component/home/home.component';
+import { LoginComponent } from './core/component/login/login.component';
+import { SignupComponent } from './core/component/signup/signup.component';
+import { AboutComponent } from './core/component/about/about.component';
+import { ContactComponent } from './core/component/contact/contact.component';
 
 const routes: Routes = [
   {
@@ -8,12 +12,27 @@ const routes: Routes = [
     loadChildren:  () => import('./portal/portal.module').then(x => x.PortalModule),
     data: { preload: false }
   },
-  { 
-    path: '', redirectTo: '/home', pathMatch: 'full' },
-      {
-        path: 'home',
-        component: HomeComponent,
-      },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+  },
 ];
 
 @NgModule({
