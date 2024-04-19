1a42d5bf48983d8514f1de945ba1b2cbc5dca325
Mado Ngome <maladongom104@gmail.com>
2024-04-19 22:26:55 +0000

    contactForm commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/contact-form/contact-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-form',
+  templateUrl: './contact-form.component.html',
+  styleUrl: './contact-form.component.css'
+})
+export class ContactFormComponent {
+
+}
