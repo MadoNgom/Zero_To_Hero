1f76a2c4bd5a5197aec79c1af86f93fc936b1a8c
Mado Ngome <maladongom104@gmail.com>
2024-05-06 12:24:22 +0000

    Home , Header and ContactForm Page

--- src/app/core/component/home/home.component.ts
+++ src/app/core/component/home/home.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
