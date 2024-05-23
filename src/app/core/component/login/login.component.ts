bcf5ca4a8e98cce390c229a248f870327cb9990c
Mado Ngome <maladongom104@gmail.com>
2024-05-23 12:08:46 +0000

    login SignUp footer

--- src/app/core/component/login/login.component.ts
+++ src/app/core/component/login/login.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import type { OnInit } from '@angular/core';
 
-/* @figmaId 4055:593 */
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+export class LoginComponent {
+
 }
---
+++ src/app/core/component/signup-form/signup-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrl: './signup-form.component.css'
+})
+export class SignupFormComponent {
+
+}
