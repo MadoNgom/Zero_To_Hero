b6140ecdd802f8dffcd918fd31b1c7d11377ddac
Mado Ngome <maladongom104@gmail.com>
2024-06-07 15:38:52 +0000

    commit Admin Dashboard

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -7,6 +7,12 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { FormsModule } from '@angular/forms';
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MainComponent } from './main/main.component';
+import { TabsModule } from '../tabs/tabs.module';
+import { TableFormateurComponent } from './components/table-formateur/table-formateur.component';
+import { TableUserComponent } from './components/table-user/table-user.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +21,12 @@ import { AdminComponent } from './admin.component';
     ManageUsersComponent,
     SettingsComponent,
     AdminComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    TableFormateurComponent,
+    TableUserComponent,
   ],
-  imports: [CommonModule, FormsModule, AdminRoutingModule],
+  imports: [CommonModule, FormsModule, AdminRoutingModule, TabsModule],
 })
 export class AdminModule {}
