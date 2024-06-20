bda7420208aecd3b75fb3511a46cc1b1361dfc24
AbdoulayeXYZ <niasseabdoulaye64@gmail.com>
2024-06-20 23:18:42 +0000

    conflicts fixed

--- src/app/models/module.model.ts
+++ src/app/models/module.model.ts
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
-import { Course } from './course.model';
-
-export class Module {
-  _id?: number;
-  name: string | undefined;
-  description: string | undefined;
-  course: Course | undefined;
-  isLock: any;
-  img: any;
-=======
 export class Module {
     _id?: string;
     name?: string;
     description?: string;
     contenu?: string;
     idCours?: string;
->>>>>>> e949af98fd5844ca568b5f37d74315fcd03e04fc
 }
